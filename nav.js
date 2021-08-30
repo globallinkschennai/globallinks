c4c42767403eb50556130b7cfa7e22aba7fc18f1
globallinkschennai <77229815+globallinkschennai@users.noreply.github.com>
2021-08-30 12:00:03 +0530

    Add files via upload

---
+++ nav.js
@@ -0,0 +1,5 @@
+document.write("<div class='navbar'><input type='checkbox' id='toggle-button'><div class='logo'> <label for='toggle-button'><span class='material-icons'>menu</span></label><h1>Global links</h1></div><div class='navbar-links'><ul><li><a href='index.html'>Home</a></li><li><a href='aboutus.html'>About Us</a></li><li class='services'><a href='service.html'>Services</a></li><li><a href='contact.html'>Contact us</a></li><li><a href='faq.html'>FaQ's</a></li></ul></div></div>")
+
+
+
+    
