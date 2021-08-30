b0dace03fa05af23af55cfd4e28e8c8dc770814e
globallinkschennai <77229815+globallinkschennai@users.noreply.github.com>
2021-08-30 12:08:27 +0530

    Add files via upload

---
+++ official.js
@@ -0,0 +1,8 @@
+function openman() {
+    var test = document.getElementById('t-containerr')
+    if (test.style.display === 'none') {
+        test.style.display = 'flex'
+    } else {
+        test.style.display = 'none'
+    }
+    }
